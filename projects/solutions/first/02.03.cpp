1c08f83c5d7d14d4f5dd7ebfbcad6a85785cca25
Mikhail Kramskoy <43179087+krabchik@users.noreply.github.com>
2025-09-30 01:41:45 +0300

    add 02.03 solution

---
+++ projects/solutions/first/02.03.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+
+int main() {
+    char c;
+    while (1)
+    {
+        std::cin >> c;
+        switch (c)
+        {
+        [[unlikely]] case '!':
+        [[unlikely]] case '"':
+        [[unlikely]] case '\'':
+        [[unlikely]] case ',':
+        [[unlikely]] case '-':
+        [[unlikely]] case '.':
+        [[unlikely]] case ':':
+        [[unlikely]] case ';':
+        [[unlikely]] case '?':
+        [[unlikely]] case '`':
+            std::cout << "punctuation" << std::endl;
+            break;
+
+        [[likely]] case '0':
+        [[likely]] case '1':
+        [[likely]] case '2':
+        [[likely]] case '3':
+        [[likely]] case '4':
+        [[likely]] case '5':
+        [[likely]] case '6':
+        [[likely]] case '7':
+        [[likely]] case '8':
+        [[likely]] case '9':
+            std::cout << "number" << std::endl;
+            break;
+
+        case 'A':
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'E':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'I':
+        case 'J':
+        case 'K':
+        case 'L':
+        case 'M':
+        case 'N':
+        case 'O':
+        case 'P':
+        case 'Q':
+        case 'R':
+        case 'S':
+        case 'T':
+        case 'U':
+        case 'V':
+        case 'W':
+        case 'X':
+        case 'Y':
+        case 'Z':
+            std::cout << "uppercase letter" << std::endl;
+            break;
+
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'i':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+        case 'n':
+        case 'o':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'u':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+            std::cout << "lowercase letter" << std::endl;
+            break;
+
+        [[likely]] default:
+            std::cout << "other" << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
